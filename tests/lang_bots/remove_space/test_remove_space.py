058d69c80d19428fc5d972b4eec80792e683fe45
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-24 05:23:16 +0300

    Move and refactor remove_space tests to new file
    Removed several test cases from test_remove_space.py and moved them to a new file, test_remove_space_part_2.py. The new file uses the implementation from hy_bot_old and includes tests for various edge cases in space removal between words and references.

--- tests/lang_bots/remove_space/test_remove_space.py
+++ tests/lang_bots/remove_space/test_remove_space.py
@@ -9,49 +9,11 @@ from src.lang_bots.remove_space import remove_spaces_between_last_word_and_begin
 class TestRemoveSpace:
     """Test cases for removing spaces between text and refs"""
 
-    def test_remove_space_end_3rd_file(self):
-        """Test removing spaces in text with multiple refs"""
-        input_text = '''            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref
-            |<!!>
-            }}</ref>։ test1 <ref name="Os2018" /><ref>{{ref
-            |<!!>
-            }}</ref>։
-        '''
-        expected = '''            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref
-            |<!!>
-            }}</ref>։ test1<ref name="Os2018" /><ref>{{ref
-            |<!!>
-            }}</ref>։
-        '''
-        result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
-        assert result == expected
-
     def test_remove_space_end_4th_file(self):
         """Test removing spaces in Armenian text with multiple refs"""
         input_text = '''            Հետծննդյան հոգեբանական խանգարումը հանդիպում է 1000 ծննդաբերությունից 1-2-ի մոտ։ <ref name="Os2018" /><ref name="Li2018" /> Տարբեր [[Մշակույթ|մշակույթներում]] և [[Դասակարգային կառուցվածք|սոցիալական դասերում]] գները նման են թվում։ <ref name="Luc2021" /> Ավելի հաճախ այն հանդիպում է հայտնի կամ նոր սկսվող երկբևեռ խանգարման համատեքստում, որը հայտնի է որպես հետծննդյան երկբևեռ խանգարում : <ref name="Luc2021" /> Այս վիճակը նկարագրվել է դեռևս մ.թ.ա. 400 թվականից [[Հիպոկրատ|Հիպոկրատի]] կողմից ։ <ref name="Os2018" />
         '''
         expected = '''            Հետծննդյան հոգեբանական խանգարումը հանդիպում է 1000 ծննդաբերությունից 1-2-ի մոտ։ <ref name="Os2018" /><ref name="Li2018" /> Տարբեր [[Մշակույթ|մշակույթներում]] և [[Դասակարգային կառուցվածք|սոցիալական դասերում]] գները նման են թվում։ <ref name="Luc2021" /> Ավելի հաճախ այն հանդիպում է հայտնի կամ նոր սկսվող երկբևեռ խանգարման համատեքստում, որը հայտնի է որպես հետծննդյան երկբևեռ խանգարում : <ref name="Luc2021" /> Այս վիճակը նկարագրվել է դեռևս մ.թ.ա. 400 թվականից [[Հիպոկրատ|Հիպոկրատի]] կողմից ։ <ref name="Os2018" />
-        '''
-        result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
-        assert result == expected
-
-    def test_remove_space_end_5th_file(self):
-        """Test removing spaces with keyword before ref"""
-        input_text = '''            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] կողմից <ref name="Os2018" /><ref>{{ref
-            |<!!>
-            }}</ref>։ կողմից <ref name="Os2018" /><ref>{{ref
-            |zz
-            }}</ref>։
-
-            == test ==
-        '''
-        expected = '''            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] կողմից <ref name="Os2018" /><ref>{{ref
-            |<!!>
-            }}</ref>։ կողմից<ref name="Os2018" /><ref>{{ref
-            |zz
-            }}</ref>։
-
-            == test ==
         '''
         result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
         assert result == expected
@@ -70,13 +32,6 @@ class TestRemoveSpace:
         result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
         assert result == expected
 
-    def test_part_4(self):
-        """Test removing spaces in complex medical text with multiple refs"""
-        input_text = 'Գոյություն ունի լյարդի ճարպային հիվանդության երկու տեսակ՝ ոչ ալկոհոլային ճարպային լյարդի հիվանդություն (ՈԱՃՀ) և ալկոհոլային լյարդի հիվանդություն <ref name="NIH2016" />։ ՈԱՃՀՀ-ն բաղկացած է պարզ ճարպային լյարդից և ոչ ալկոհոլային ստեատոհեպատիտից (ՈԱՃՀ): <ref name="AFP2013">{{Cite journal|last=Iser|first=D|last2=Ryan|first2=M|title=Fatty liver disease—a practical guide for GPs.|journal=Australian Family Physician|date=July 2013|volume=42|issue=7|pages=444–7|pmid=23826593}}</ref><ref name="NIH2016" /> Հիմնական ռիսկերից են [[Էթիլ սպիրտ|ալկոհոլը]], [[Տիպ 2 շաքարային դիաբետ|2-րդ տիպի շաքարախտը]] և [[Ճարպակալում|ճարպակալումը]] <ref name="Ant2019" /><ref name="NIH2016" />։ Այլ ռիսկի գործոններից են որոշակի դեղամիջոցները, ինչպիսիք են [[Գլյուկոկորտիկոիդներ|գլյուկոկորտիկոիդները]] և [[Հեպատիտ C|հեպատիտ C-ն]] : <ref name="NIH2016" /> Անհասկանալի է, թե ինչու են ոչ ալկոհոլային ճարպային լյարդի հիվանդություն ունեցող որոշ մարդիկ զարգացնում պարզ ճարպային լյարդ, իսկ մյուսները՝ ոչ ալկոհոլային հեպատիտ: <ref name="NIH2016" /> Ախտորոշումը հիմնված է [[Անամնեզ|բժշկական պատմության]] վրա, որը հաստատվում է արյան անալիզներով, բժշկական պատկերագրական հետազոտություններով և երբեմն լյարդի բիոպսիայով <ref name="NIH2016" />։'
-        expected = 'Գոյություն ունի լյարդի ճարպային հիվանդության երկու տեսակ՝ ոչ ալկոհոլային ճարպային լյարդի հիվանդություն (ՈԱՃՀ) և ալկոհոլային լյարդի հիվանդություն <ref name="NIH2016" />։ ՈԱՃՀՀ-ն բաղկացած է պարզ ճարպային լյարդից և ոչ ալկոհոլային ստեատոհեպատիտից (ՈԱՃՀ): <ref name="AFP2013">{{Cite journal|last=Iser|first=D|last2=Ryan|first2=M|title=Fatty liver disease—a practical guide for GPs.|journal=Australian Family Physician|date=July 2013|volume=42|issue=7|pages=444–7|pmid=23826593}}</ref><ref name="NIH2016" /> Հիմնական ռիսկերից են [[Էթիլ սպիրտ|ալկոհոլը]], [[Տիպ 2 շաքարային դիաբետ|2-րդ տիպի շաքարախտը]] և [[Ճարպակալում|ճարպակալումը]] <ref name="Ant2019" /><ref name="NIH2016" />։ Այլ ռիսկի գործոններից են որոշակի դեղամիջոցները, ինչպիսիք են [[Գլյուկոկորտիկոիդներ|գլյուկոկորտիկոիդները]] և [[Հեպատիտ C|հեպատիտ C-ն]] : <ref name="NIH2016" /> Անհասկանալի է, թե ինչու են ոչ ալկոհոլային ճարպային լյարդի հիվանդություն ունեցող որոշ մարդիկ զարգացնում պարզ ճարպային լյարդ, իսկ մյուսները՝ ոչ ալկոհոլային հեպատիտ: <ref name="NIH2016" /> Ախտորոշումը հիմնված է [[Անամնեզ|բժշկական պատմության]] վրա, որը հաստատվում է արյան անալիզներով, բժշկական պատկերագրական հետազոտություններով և երբեմն լյարդի բիոպսիայով<ref name="NIH2016" />։'
-        result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
-        assert result == expected
-
 
 class TestRemoveSpaceBetweenLastWordAndRef:
     """Test cases for removing spaces between last word and beginning of ref"""
@@ -85,26 +40,5 @@ class TestRemoveSpaceBetweenLastWordAndRef:
         """Test basic space removal before ref"""
         input_text = 'Այն առաջին անգամ արձանագրվել է 1750 թվականին Ամերիկայում գտնվող քահանա Օլիվեր Հարթի օրագրային գրառման մեջ  <ref name="Sc2011">{{Cite book|last=Schachner}}</ref>։'
         expected = 'Այն առաջին անգամ արձանագրվել է 1750 թվականին Ամերիկայում գտնվող քահանա Օլիվեր Հարթի օրագրային գրառման մեջ<ref name="Sc2011">{{Cite book|last=Schachner}}</ref>։'
-        result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
-        assert result == expected
-
-    def test_remove_space_with_colon(self):
-        """Test space removal before ref with colon punctuation"""
-        input_text = 'Բուժումը ներառում է [[Թերապիա (բուժում)|օժանդակ միջոցառումներ]] <ref name="NORD2004" /> : Ոսկրային որոշակի անոմալիաներ շտկելու համար կարող է իրականացվել վիրահատություն <ref name="GARD2016" /> :'
-        expected = 'Բուժումը ներառում է [[Թերապիա (բուժում)|օժանդակ միջոցառումներ]]<ref name="NORD2004" />: Ոսկրային որոշակի անոմալիաներ շտկելու համար կարող է իրականացվել վիրահատություն<ref name="GARD2016" />:'
-        result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
-        assert result == expected
-
-    def test_remove_space_multiple_punctuations(self):
-        """Test multiple punctuation marks in text"""
-        input_text = 'Գոյություն ունի լյարդի ճարպային հիվանդություն <ref name="NIH2016" />։ ՈԱՃՀՀ-ն բաղկացած է <ref name="AFP2013">{{Cite journal}}</ref><ref name="NIH2016" /> Հիմնական ռիսկերից են <ref name="Ant2019" /><ref name="NIH2016" />։'
-        expected = 'Գոյություն ունի լյարդի ճարպային հիվանդություն <ref name="NIH2016" />։ ՈԱՃՀՀ-ն բաղկացած է <ref name="AFP2013">{{Cite journal}}</ref><ref name="NIH2016" /> Հիմնական ռիսկերից են <ref name="Ant2019" /><ref name="NIH2016" />։'
-        result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
-        assert result == expected
-
-    def test_remove_space_end_with_links(self):
-        """Test with wiki links"""
-        input_text = 'Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref|<!!>}}</ref>։ test1 <ref name="Os2018" /><ref>{{ref|<!!>}}</ref>։'
-        expected = 'Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref|<!!>}}</ref>։ test1<ref name="Os2018" /><ref>{{ref|<!!>}}</ref>։'
         result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
         assert result == expected
---
+++ tests/lang_bots/remove_space/test_remove_space_part_2.py
@@ -0,0 +1,81 @@
+"""Tests for remove_space (remove_spaceTest.php)
+
+Converted from tests/remove_spaceTest.php
+"""
+import pytest
+# from src.lang_bots.remove_space import remove_spaces_between_last_word_and_beginning_of_ref
+from src.lang_bots.hy_bot_old import remove_spaces_between_last_word_and_beginning_of_ref
+
+
+def test_remove_space_end_3rd_file():
+    """Test removing spaces in text with multiple refs"""
+    input_text = '''            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref
+        |<!!>
+        }}</ref>։ test1 <ref name="Os2018" /><ref>{{ref
+        |<!!>
+        }}</ref>։
+    '''
+    expected = '''            Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref
+        |<!!>
+        }}</ref>։ test1<ref name="Os2018" /><ref>{{ref
+        |<!!>
+        }}</ref>։
+    '''
+    result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
+    assert result == expected
+
+
+def test_remove_space_end_5th_file() -> None:
+    """Test removing spaces with keyword before ref"""
+    input_text = '''
+        Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] կողմից <ref name="Os2018" /><ref>{{ref
+        |<!!>
+        }}</ref>։ կողմից <ref name="Os2018" /><ref>{{ref
+        |zz
+        }}</ref>։
+
+        == test ==
+    '''
+    expected = '''
+        Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] կողմից <ref name="Os2018" /><ref>{{ref
+        |<!!>
+        }}</ref>։ կողմից<ref name="Os2018" /><ref>{{ref
+        |zz
+        }}</ref>։
+
+        == test ==
+    '''
+    result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
+    assert result == expected
+
+
+def test_remove_space_end_with_links():
+    """Test with wiki links"""
+    input_text = 'Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref|<!!>}}</ref>։ test1 <ref name="Os2018" /><ref>{{ref|<!!>}}</ref>։'
+    expected = 'Article text <ref>{{Citar web|Text|author=John|language=en}}</ref> [[Հիպոկրատ|Հիպոկրատի]] test0 <ref name="Os2018" /><ref>{{ref|<!!>}}</ref>։ test1<ref name="Os2018" /><ref>{{ref|<!!>}}</ref>։'
+    result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
+    assert result == expected
+
+
+def test_remove_space_multiple_punctuations():
+    """Test multiple punctuation marks in text"""
+    input_text = 'Գոյություն ունի լյարդի ճարպային հիվանդություն <ref name="NIH2016" />։ ՈԱՃՀՀ-ն բաղկացած է <ref name="AFP2013">{{Cite journal}}</ref><ref name="NIH2016" /> Հիմնական ռիսկերից են <ref name="Ant2019" /><ref name="NIH2016" />։'
+    expected = 'Գոյություն ունի լյարդի ճարպային հիվանդություն <ref name="NIH2016" />։ ՈԱՃՀՀ-ն բաղկացած է <ref name="AFP2013">{{Cite journal}}</ref><ref name="NIH2016" /> Հիմնական ռիսկերից են <ref name="Ant2019" /><ref name="NIH2016" />։'
+    result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
+    assert result == expected
+
+
+def test_remove_space_with_colon():
+    """Test space removal before ref with colon punctuation"""
+    input_text = 'Բուժումը ներառում է [[Թերապիա (բուժում)|օժանդակ միջոցառումներ]] <ref name="NORD2004" /> : Ոսկրային որոշակի անոմալիաներ շտկելու համար կարող է իրականացվել վիրահատություն <ref name="GARD2016" /> :'
+    expected = 'Բուժումը ներառում է [[Թերապիա (բուժում)|օժանդակ միջոցառումներ]]<ref name="NORD2004" />: Ոսկրային որոշակի անոմալիաներ շտկելու համար կարող է իրականացվել վիրահատություն<ref name="GARD2016" />:'
+    result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
+    assert result == expected
+
+
+def test_part_4():
+    """Test removing spaces in complex medical text with multiple refs"""
+    input_text = 'Գոյություն ունի լյարդի ճարպային հիվանդության երկու տեսակ՝ ոչ ալկոհոլային ճարպային լյարդի հիվանդություն (ՈԱՃՀ) և ալկոհոլային լյարդի հիվանդություն <ref name="NIH2016" />։ ՈԱՃՀՀ-ն բաղկացած է պարզ ճարպային լյարդից և ոչ ալկոհոլային ստեատոհեպատիտից (ՈԱՃՀ): <ref name="AFP2013">{{Cite journal|last=Iser|first=D|last2=Ryan|first2=M|title=Fatty liver disease—a practical guide for GPs.|journal=Australian Family Physician|date=July 2013|volume=42|issue=7|pages=444–7|pmid=23826593}}</ref><ref name="NIH2016" /> Հիմնական ռիսկերից են [[Էթիլ սպիրտ|ալկոհոլը]], [[Տիպ 2 շաքարային դիաբետ|2-րդ տիպի շաքարախտը]] և [[Ճարպակալում|ճարպակալումը]] <ref name="Ant2019" /><ref name="NIH2016" />։ Այլ ռիսկի գործոններից են որոշակի դեղամիջոցները, ինչպիսիք են [[Գլյուկոկորտիկոիդներ|գլյուկոկորտիկոիդները]] և [[Հեպատիտ C|հեպատիտ C-ն]] : <ref name="NIH2016" /> Անհասկանալի է, թե ինչու են ոչ ալկոհոլային ճարպային լյարդի հիվանդություն ունեցող որոշ մարդիկ զարգացնում պարզ ճարպային լյարդ, իսկ մյուսները՝ ոչ ալկոհոլային հեպատիտ: <ref name="NIH2016" /> Ախտորոշումը հիմնված է [[Անամնեզ|բժշկական պատմության]] վրա, որը հաստատվում է արյան անալիզներով, բժշկական պատկերագրական հետազոտություններով և երբեմն լյարդի բիոպսիայով <ref name="NIH2016" />։'
+    expected = 'Գոյություն ունի լյարդի ճարպային հիվանդության երկու տեսակ՝ ոչ ալկոհոլային ճարպային լյարդի հիվանդություն (ՈԱՃՀ) և ալկոհոլային լյարդի հիվանդություն <ref name="NIH2016" />։ ՈԱՃՀՀ-ն բաղկացած է պարզ ճարպային լյարդից և ոչ ալկոհոլային ստեատոհեպատիտից (ՈԱՃՀ): <ref name="AFP2013">{{Cite journal|last=Iser|first=D|last2=Ryan|first2=M|title=Fatty liver disease—a practical guide for GPs.|journal=Australian Family Physician|date=July 2013|volume=42|issue=7|pages=444–7|pmid=23826593}}</ref><ref name="NIH2016" /> Հիմնական ռիսկերից են [[Էթիլ սպիրտ|ալկոհոլը]], [[Տիպ 2 շաքարային դիաբետ|2-րդ տիպի շաքարախտը]] և [[Ճարպակալում|ճարպակալումը]] <ref name="Ant2019" /><ref name="NIH2016" />։ Այլ ռիսկի գործոններից են որոշակի դեղամիջոցները, ինչպիսիք են [[Գլյուկոկորտիկոիդներ|գլյուկոկորտիկոիդները]] և [[Հեպատիտ C|հեպատիտ C-ն]] : <ref name="NIH2016" /> Անհասկանալի է, թե ինչու են ոչ ալկոհոլային ճարպային լյարդի հիվանդություն ունեցող որոշ մարդիկ զարգացնում պարզ ճարպային լյարդ, իսկ մյուսները՝ ոչ ալկոհոլային հեպատիտ: <ref name="NIH2016" /> Ախտորոշումը հիմնված է [[Անամնեզ|բժշկական պատմության]] վրա, որը հաստատվում է արյան անալիզներով, բժշկական պատկերագրական հետազոտություններով և երբեմն լյարդի բիոպսիայով<ref name="NIH2016" />։'
+    result = remove_spaces_between_last_word_and_beginning_of_ref(input_text, 'hy')
+    assert result == expected
