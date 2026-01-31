c6a607ea930cb2386e0402acf21352a915570dc3
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-01-31 10:50:40 +0300

    Update version number to 0.1.0

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ fix_one_page - WikiText reference/category/template fixer
 A Python library to parse and fix references in MediaWiki wikitext.
 """
 
-__version__ = "1.0.0"
+__version__ = "0.1.0"
 
 from .core.fix_page import fix_one_page
 from .core import DoChangesToText1
